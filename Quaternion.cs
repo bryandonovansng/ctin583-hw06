019c0f254182ba1eb2c9d34bf46df676e47aae41
bryandonovansng <sngyong@usc.edu>
2024-02-16 21:18:06 -0800

    my homework

--- Quaternion.cs
+++ Quaternion.cs
@@ -13,9 +13,15 @@ animate our cube to rotate with Quaternions.
 public class Quaternion : MonoBehaviour
 {
     // TODO: Problem 1: Define Vector3 and Rotation variables here.
-
+    public Vector3 rotationAxis = Vector3.up; // Default axis is Y-axisn
+    public int rotationAngle = 15; //setting angle of rotation
+    public Vector3 startRotation = (0, 0, 0); //setting the start rotation 
+    public Vector3 endRotation = (100, 100, 100); //setting the end rotation 
+    
     // TODO: Problem 2: Define two Quaternions
+    public Quaternion quaternion1 = Quaternion.identity;
 
+    public Quaternion quaternion2 = Quaternion.identity;
     // Update is called once per frame
     void Update()
     {
@@ -23,12 +29,24 @@ public class Quaternion : MonoBehaviour
         result = Quaternion.Euler(0, rotation, 0) * result;
 
         // TODO: Problem 3: Multiply a Quaternion by another Quaternion using Quaternion.Euler and save it as a temporary variable
+        quaternion1 = Quaternion.Euler(rotationAxis * rotationAngle);
+        quaternion2 = Quaternion.Euler(rotationAxis * rotationAngle);
+        Quaternion tempQuaternion = quaternion1 * quaternion2;
 
         // TODO: Problem 4: Check if two quaternions are equal to each other. 
         // If they are, print out the "[Names of Quaternions] are Equal".
         // Else, print out "Quaternions are different"
+        if (quaternion1 == quaternion2)
+        {
+            Debug.Log("quaternion 1 and quaternion 2 are Equal")
+        }
+        else
+        {
+            Debug.Log("quaternions are different")
+        }
 
         // TODO: Problem 5: Using Quaternion.Lerp, rotate your vector
         // The vector starts at (0,0,0) and wants to be rotated to (100, 100, 100)
+        Quaternion.Lerp(startRotation, endRotation, 1);
     }
 }
